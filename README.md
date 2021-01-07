ec69f77c65d016d7a84ebbea739bcea5f35070e2
c43721 <rlahman.game@gmail.com>
2021-01-06 18:08:31 -0600

    Update script, readme. Include warnings about migration.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # website-rcon
 Lets you exec RCON from a website.
 
-## Depracated. Use https://rcon.tf instead with their interactive dashboard and their easy-to-use web-rcon.
+## Deprecated. Use https://rcon.tf instead with their interactive dashboard and their easy-to-use web-rcon.
+
+## To be moved officially to the rcon.tf domain by July, 2021.
--- magic.js
+++ magic.js
@@ -1,31 +1,55 @@
-const ipElement = document.getElementById("ip-field")
-const rconElement = document.getElementById("rcon-field")
-const cmdElement = document.getElementById("command-field")
-const resultElement = document.getElementById("results")
-const submitButton = document.getElementById("submit-button-post")
+const ipElement = document.getElementById("ip-field");
+const portElement = document.getElementById("port-field");
+const rconElement = document.getElementById("rcon-field");
+const cmdElement = document.getElementById("command-field");
+const resultElement = document.getElementById("results");
+const submitButton = document.getElementById("submit-button-post");
 
-submitButton.addEventListener('click', async ev => {
-    ev.preventDefault()
+const API_URL = "https://rcon.tf/api/execute";
 
-    const result = await fetch('https://payload.tf/api/external/rcon', {
-        method: "POST",
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            ip: ipElement.value,
-            password: rconElement.value,
-            command: cmdElement.value
-        })
-    })
+submitButton.addEventListener("click", async ev => {
+    ev.preventDefault();
+    
+    
+	try {
+        const startTime = window.performance.now();
+        console.log("%c[rcon.tf] %cSending request to %crcon.tf...", 'color: #fb8c00;', 'color: #fff;', 'color: #fb8c00;');
+        const { body, ...response } = await sendRequest();
+        const endTime = window.performance.now();
+        console.log(`%c[rcon.tf] %cRecieved response from %crcon.tf %cin ${(endTime - startTime).toFixed(0)} ms`, 'color: #fb8c00;', 'color: #fff;', 'color: #fb8c00;', 'color: #fff;');
 
-    const resJson = await result.json()
+        if (cmdElement.value.includes("changelevel"))
+            return (resultElement.innerText = `Changed level to: ${cmdElement.value.slice(12)}`);
+            
+		if (!body) {
+            console.log(`%c[rcon.tf] %cRecieved bad response from %crcon.tf%c:\nCode: ${response.statusCode}\nMessage: ${response.message}\nError: ${response.error}`, 'color: #fb8c00;', 'color: #fff;', 'color: #fb8c00;', 'color: #fff;');
+			return (resultElement.innerText = `${response.error}\n${response.message}`);
+		}
 
-    if (resJson.success && cmdElement.value.includes("changelevel")) {
-        resultElement.innerText = `Changed level to: ${cmdElement.value.slice(12)}`
-    } else if (resJson.success) {
-        resultElement.innerText = JSON.stringify(resJson.response).replace(/\\n/g, "\n").slice(1, -1).replace(/[\\\"]/g, "%QUOTE").replace(/%QUOTE%QUOTE/g, "\"")
-    } else alert(JSON.stringify(resJson))
-})
+		return (resultElement.innerText = JSON.stringify(body)
+			.replace(/\\n/g, "\n")
+			.slice(1, -1)
+			.replace(/[\\\"]/g, "%QUOTE")
+			.replace(/%QUOTE%QUOTE/g, '"'));
+	} catch (err) {
+		return (resultElement.innerText = `Error while executing command.`);
+	}
+});
 
+async function sendRequest() {
+	const res = await fetch(API_URL, {
+		method: "POST",
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify({
+			ip: ipElement.value,
+			password: rconElement.value,
+            command: cmdElement.value,
+            port: parseInt(portElement.value, 10)
+		})
+	});
+
+	return await res.json();
+}
